cbb69cc021b7ba36474718a4bc85bb594303433b
victoriacuba01@gamil.com <victoriacuba01@gmail.com>
2022-12-05 22:36:57 -0300

    Agrego tp2

---
+++ Clase_2_Ejemplo_2.java
@@ -0,0 +1,17 @@
+/*Ejemplo de declaracion de variables y asignacion de * valores*/
+
+public class Clase_2_Ejemplo_2 {
+	public static void main(String[] args) {
+		//Antes de usar variables hay q declararlas
+		int edad;
+		double altura, peso;
+		boolean existe;
+		//las variables declaradas solo pueden tomar valores segun los tipos declarados
+		edad= 30;
+		altura= 1.7;
+		existe = false;
+		peso = 75.5;
+
+	}
+
+}
